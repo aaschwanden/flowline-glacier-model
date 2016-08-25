c98e60a2ba685d2117d5fb75b81e3a573f4845a6
Andy Aschwanden <aaschwanden@alaska.edu>
2016-08-24 17:33:27 -0800

    more work on debugging the LT code

--- linear_orog_precip.py
+++ linear_orog_precip.py
@@ -5,12 +5,15 @@ Created on Tue Apr  7 18:47:27 2015
 @author: leif
 """
 
-
+import logging
 from scipy.fftpack import fft2, fftfreq
 import numpy as np
 import cmath
 from osgeo import gdal, osr
 
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class OrographicPrecipitation(object):
 
@@ -27,7 +30,7 @@ class OrographicPrecipitation(object):
         self.physical_constants = physical_constants
         self.dx = np.diff(X)[0, 0]
         self.dy = np.diff(Y, axis=0)[0, 0]
-        self.nx = len(Orography[1,:])
+        self.nx = len(Orography[1, :])
         self.ny = len(Orography)
 
         self.P = self._compute_precip(ounits)
@@ -35,7 +38,7 @@ class OrographicPrecipitation(object):
             self.P_units = ounits
         else:
             self.P_units = 'mm hr-1'
-        
+
     def _compute_precip(self, ounits):
 
         physical_constants = self.physical_constants
@@ -47,12 +50,12 @@ class OrographicPrecipitation(object):
         ny = self.ny
         U = self.U
         V = self.V
-        
-        x_n_value = np.fft.fftfreq(len(Orography[1,:]), (1.0 / len(Orography[1,:])))
-        y_n_value = np.fft.fftfreq(len(Orography), (1.0/len(Orography)))
+
+        x_n_value = np.fft.fftfreq(len(Orography[1, :]), (1.0 / len(Orography[1, :])))
+        y_n_value = np.fft.fftfreq(len(Orography), (1.0 / len(Orography)))
 
         x_len = len(Orography) * dx
-        y_len = len(Orography[1,:]) * dy
+        y_len = len(Orography[1, :]) * dy
 
         kx_line = np.divide(np.multiply(2.0 * np.pi, x_n_value), x_len)
         ky_line = np.divide(np.multiply(2.0 * np.pi, y_n_value), y_len)[np.newaxis].T
@@ -67,39 +70,51 @@ class OrographicPrecipitation(object):
         # Eqn. 12
         m_denom = np.power(sigma, 2.) - physical_constants['f']**2
         m_reg = 1e-18
-        m_denom[np.abs(m_denom)<m_reg] = m_reg
+        m_denom[np.logical_and((np.abs(np.real(m_denom)) < m_reg), (np.abs(np.real(m_denom)) >= 0))] = m_reg
+        m_denom[np.logical_and((np.abs(np.real(m_denom)) < m_reg), (np.abs(np.real(m_denom)) < 0))] = -m_reg
+        # m_denom[np.abs(m_denom) > m_reg] = -m_reg
+
         m1 = np.divide(np.subtract(physical_constants['Nm']**2, np.power(sigma, 2.)), m_denom)
         m2 = np.add(np.power(kx, 2.), np.power(ky, 2.))
         m_sqr = np.multiply(m1, m2)
         m = np.zeros_like(m_sqr)
-        m[m_sqr>0] = np.sqrt(m_sqr[m_sqr>0])
-        m[m_sqr<0] = np.sqrt(-m_sqr[m_sqr<0])
+        m[m_sqr > 0] = np.sqrt(m_sqr[m_sqr > 0])
+        m[m_sqr < 0] = np.sqrt(-m_sqr[m_sqr < 0])
+        m[sigma == 0] = np.sqrt(m_sqr[sigma == 0]) * np.sign(sigma[sigma == 0])
+        # m = np.maximum(m,0.0001)
+        # m = np.minimum(m,0.01)
+        # m[:, 0:15] = 0
+        # m[:, -14:-1] = 0
         
         # Numerator in Eqn. 49
         P_karot_num = np.multiply(np.multiply(np.multiply(physical_constants['Cw'], cmath.sqrt(-1)), sigma), Orography_fft)
         P_karot_denom_Hw = np.subtract(np.multiply(np.multiply(physical_constants['Hw'], m), cmath.sqrt(-1)), 1)
-        P_karot_denom_tauc =  np.add(np.multiply(np.multiply(sigma, physical_constants['tau_c']), cmath.sqrt(-1)), 1)
+        P_karot_denom_tauc = np.add(np.multiply(np.multiply(sigma, physical_constants['tau_c']), cmath.sqrt(-1)), 1)
         P_karot_denom_tauf = np.add(np.multiply(np.multiply(sigma, physical_constants['tau_f']), cmath.sqrt(-1)), 1)
         # Denominator in Eqn. 49
         P_karot_denom = np.multiply(P_karot_denom_Hw, np.multiply(P_karot_denom_tauc, P_karot_denom_tauf))
-        P_karot = np.divide(P_karot_num, P_karot_denom)      
+        P_karot = np.divide(P_karot_num, P_karot_denom)
 
         P_karot_amp = np.absolute(P_karot)  # get the amplitude
         P_karot_angle = np.angle(P_karot)   # get the phase angle
 
         # Converting from wave domain back to space domain
         # Eqn. 6
-        y2 = np.multiply(P_karot_amp,  np.add(np.cos(P_karot_angle), np.multiply(cmath.sqrt(-1), np.sin(P_karot_angle))))
-        y3 = np.fft.ifft2(y2)
+        # y2 = np.multiply(P_karot_amp, np.add(np.cos(P_karot_angle), np.multiply(cmath.sqrt(-1), np.sin(P_karot_angle))))
+        # y3 = np.fft.ifft2(y2)
+        y3 = np.fft.ifft2(P_karot)
         spy = 31556925.9747
         P = np.multiply(np.real(y3), 3600)   # mm hr-1
         # Truncation
         P[P < 0] = 0
         # Add background precip
-        P +=  physical_constants['P0']
+        P += physical_constants['P0']
         if physical_constants['Pscale'] is not None:
             P *= physical_constants['Pscale']
 
+
+        self.m = m
+        self.P_karot = P_karot
         if ounits is not None:
             import cf_units
             in_units = cf_units.Unit('mm hr-1')
@@ -176,8 +191,8 @@ def array2raster(newRasterfn, geoTrans, proj4, units, array):
     outRasterSRS.ImportFromProj4(proj4)
     outRaster.SetProjection(outRasterSRS.ExportToWkt())
     outband.FlushCache()
-    
-        
+
+
 if __name__ == "__main__":
     print('Linear Orographic Precipitation Model by Smith & Barstad (2004)')
 
@@ -192,22 +207,22 @@ if __name__ == "__main__":
     parser.add_argument('-o', dest='out_file',
                         help='Output file', default='foo.nc')
     parser.add_argument('--latitude', dest='lat', type=float,
-                        help='Latitude to compute Coriolis term.', default=45.)
+                        help='Latitude to compute Coriolis term.', default=0.)
     parser.add_argument('--wind_direction', dest='direction', type=float,
-                        help='Direction from which the wind is coming.', default=249.1)
+                        help='Direction from which the wind is coming.', default=270)
     parser.add_argument('--wind_magnitude', dest='magnitude', type=float,
-                        help='Magnitude of wind velocity [m/s].', default=22.1)
+                        help='Magnitude of wind velocity [m/s].', default=15)
     parser.add_argument('--tau_c', dest='tau_c', type=float,
-                        help='conversion time [s].', default=933.)
+                        help='conversion time [s].', default=1000)
     parser.add_argument('--tau_f', dest='tau_f', type=float,
-                        help='fallout time [s].', default=933.)
+                        help='fallout time [s].', default=1000)
     parser.add_argument('--moist_stability', dest='Nm', type=float,
-                        help='moist stability frequency [s-1].', default=0.0024)
+                        help='moist stability frequency [s-1].', default=0.005)
     parser.add_argument('--vapor_scale_height', dest='Hw', type=float,
                         help='Water vapor scale height [m].', default=2500)
     parser.add_argument('--background_precip', dest='P0', type=float,
                         help='Background precipitation rate [m/s].', default=0.)
-    
+
     options = parser.parse_args()
     in_file = options.in_file
     out_file = options.out_file
@@ -241,18 +256,18 @@ if __name__ == "__main__":
     physical_constants = dict()
     physical_constants['tau_c'] = tau_c      # conversion time [s]
     physical_constants['tau_f'] = tau_f      # fallout time [s]
-    physical_constants['f'] =  2* 7.2921e-5*np.sin(lat*np.pi/180) # Coriolis force
-    physical_constants['Nm'] = Nm       # moist stability frequency [s-1]
+    physical_constants['f'] = 2 * 7.2921e-5 * np.sin(lat * np.pi / 180) # Coriolis force
+    physical_constants['Nm'] = Nm   # moist stability frequency [s-1]
     physical_constants['Cw'] = rho_Sref * Theta_m / gamma # uplift sensitivity factor [kg m-3]
-    # physical_constants['Cw'] = 1 # uplift sensitivity factor [kg m-3]
+    #physical_constants['Cw'] = 0.02 # uplift sensitivity factor [kg m-3]
     physical_constants['Hw'] = Hw         # vapor scale height
     physical_constants['u'] = np.sin(direction*2*np.pi/360) * magnitude    # x-component of wind vector [m s-1]
     physical_constants['v'] = -np.cos(direction*2*np.pi/360) * magnitude   # y-component of wind vector [m s-1]
     physical_constants['P0'] = P0   # background precip [m s-1]
     physical_constants['Pscale'] = None   # scale factor
 
-    U = np.multiply(np.ones( (len(Orography), len(Orography[1,:])), dtype = float), physical_constants['u'])
-    V = np.multiply(np.ones( (len(Orography), len(Orography[1,:])), dtype = float), physical_constants['v'])
+    U = np.multiply(np.ones((len(Orography), len(Orography[1, :])), dtype=float), physical_constants['u'])
+    V = np.multiply(np.ones((len(Orography), len(Orography[1, :])), dtype=float), physical_constants['v'])
 
     OP = OrographicPrecipitation(X, Y, U, V, Orography, physical_constants)
     P = OP.P
@@ -260,6 +275,13 @@ if __name__ == "__main__":
 
     if in_file is not None:
         array2raster(out_file, gd.geoTrans, gd.proj4, units, P)
-
-
-
+    else:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        c = ax.pcolormesh(X, Y, P)
+        plt.colorbar(c)
+        ax.contour(X, Y, P, np.arange(0.025, 2.425, 0.4), colors='1.')
+        ax.contour(X, Y, Orography)
+        ax.set_xlim(-100e3, 200e3)
+        ax.set_ylim(-150e3, 150e3)
+        plt.show()
